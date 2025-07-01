5d516eae4c01f6ece3ba2c6353be49f71c56775f
박의현 <uihyun420@gmail.com>
2025-07-01 10:15:13 +0900

    벌움직임캐릭터추가

--- Bee.h
+++ Bee.h
@@ -1,26 +1,26 @@
-#pragma once
-#include "GameObject.h"
-class Bee :
-    public GameObject
-{
-protected:
-	sf::Sprite bee;	
-	std::string texIdBee;
-	float speed = 0.f;
-	sf::Vector2f direction;
-
-public:
-	Bee() = default;
-	~Bee() override = default;
-
-	void SetPosition(const sf::Vector2f& pos) override;
-	
-	void SetSide(Sides side);
-
-	void Init() override;
-	void Release() override;
-	void Reset() override;
-	void Update(float dt) override;
-	void Draw(sf::RenderWindow& window) override;
-};
-
+//#pragma once
+//#include "GameObject.h"
+//class Bee :
+//    public GameObject
+//{
+//protected:
+//	sf::Sprite bee;	
+//	std::string texIdBee;
+//	float speed = 0.f;
+//	sf::Vector2f direction;
+//
+//public:
+//	Bee() = default;
+//	~Bee() override = default;
+//
+//	void SetPosition(const sf::Vector2f& pos) override;
+//	
+//	void SetSide(Sides side);
+//
+//	void Init() override;
+//	void Release() override;
+//	void Reset() override;
+//	void Update(float dt) override;
+//	void Draw(sf::RenderWindow& window) override;
+//};
+//
